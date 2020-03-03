ed0b7b408397047959a236086847ba3a0e72e657
mcagriardic <cagri_ardic@hotmail.com>
2020-03-03 16:44:07 +0000

    Added blur

--- Week4/filter/helpers.c
+++ Week4/filter/helpers.c
@@ -15,18 +15,6 @@ void PixelsAveraged(BYTE *rgbtRed, BYTE *rgbtGreen, BYTE *rgbtBlue)
     *rgbtBlue = averaged_rounded;
 }
 
-void PixelReflected(int row, int column, int width, RGBTRIPLE image[][width])
-{
-    int reflected_column = width - 1 - column;
-    RGBTRIPLE(*temp) = calloc(1, sizeof(RGBTRIPLE));
-
-    *temp = image[row][reflected_column];
-    image[row][reflected_column] = image[row][column];
-    image[row][column] = *temp;
-
-
-}
-
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     // printf("Height: %i - width: %i\n", height, width);
@@ -43,6 +31,16 @@ void grayscale(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+void PixelReflected(int row, int column, int width, RGBTRIPLE image[][width])
+{
+    int reflected_column = width - 1 - column;
+    RGBTRIPLE(*temp) = calloc(1, sizeof(RGBTRIPLE));
+
+    *temp = image[row][reflected_column];
+    image[row][reflected_column] = image[row][column];
+    image[row][column] = *temp;
+}
+
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
@@ -50,7 +48,7 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     // FILE *bf = fopen("before.txt", "w");
     for (int row=0; row < height; row++)
     {
-        printf("At row: %i ...\n", row);
+        // printf("At row: %i ...\n", row);
         for (int column=0; column < width/2; column++)
         {
             // printf("BEFORE SWITCH INSIDE REFLECT ...\n");
@@ -67,9 +65,58 @@ void reflect(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+RGBTRIPLE SurroundedBoxesAveraged(int row, int column, int height, int width, RGBTRIPLE image[][width], RGBTRIPLE temp[][width])
+{
+    int unsigned bottom = (row + 1) >= height ? height-1 : (row + 1);
+    int unsigned top = (row - 1) < 0 ? 0 : (row - 1);
+
+    int unsigned right = (column + 1) >= width ? width-1 : (column + 1);
+    int unsigned left = (column - 1) < 0 ? 0: (column - 1);
+
+    int unsigned denominator = (bottom - top + 1) * (right-left + 1) - 1;
+
+    float average_red = 0;
+    float average_green = 0;
+    float average_blue = 0;
+    for (int i=0;  i <= bottom - top; i++)
+    {
+        for (int j = 0; j <= right - left; j++)
+        {
+            if (top + i != row || left + j != column)
+            {
+                average_red += image[top + i][left + j].rgbtRed;
+                average_green += image[top + i][left + j].rgbtGreen;
+                average_blue += image[top + i][left + j].rgbtBlue;
+            }
+        }
+    }
+    temp[row][column].rgbtRed = (int)round(average_red / denominator);
+    temp[row][column].rgbtGreen = (int)round(average_green / denominator);
+    temp[row][column].rgbtBlue = (int)round(average_blue / denominator);
+
+    return temp[row][column];
+}
+
 // Blur image
 void blur(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE(*temp)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    for (int row=0; row < height; row++)
+    {
+        for (int column=0; column < width; column++)
+        {
+            temp[row][column] = SurroundedBoxesAveraged(row, column, height, width, image, temp);
+        }
+    }
+    
+    for (int i=0; i < height; i++)
+    {
+        for (int j=0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
+
     return;
 }
 
