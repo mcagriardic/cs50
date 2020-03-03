af173b2a8c6ed0c14887d4b10cac0ba461b94574
mcagriardic <cagri_ardic@hotmail.com>
2020-03-03 19:06:25 +0000

    Added edge detection - week 4

--- Week4/filter/helpers.c
+++ Week4/filter/helpers.c
@@ -4,7 +4,22 @@
 #include <stdlib.h>
 #include <math.h>
 
-// Convert image to grayscale
+// FILE *af = fopen("after.txt", "w");
+// FILE *bf = fopen("before.txt", "w");
+// fprintf(bf, "(%i)=%i-%i-%i,",column, image[row][column].rgbtRed, image[row][column].rgbtGreen, image[row][column].rgbtBlue);
+// fprintf(af, "(%i)=%i-%i-%i,",width-column-1, image[row][column].rgbtRed, image[row][column].rgbtGreen, image[row][column].rgbtBlue);
+// fclose(af);
+// fclose(bf);
+
+int Gx[3][3] = {
+    {-1,0,1},
+    {-2,0,2},
+    {-1,0,1}};
+
+int Gy[3][3] = {
+    {-1,-2,-1},
+    {0,0,0},
+    {1,2,1}};
 
 void PixelsAveraged(BYTE *rgbtRed, BYTE *rgbtGreen, BYTE *rgbtBlue)
 {
@@ -15,6 +30,7 @@ void PixelsAveraged(BYTE *rgbtRed, BYTE *rgbtGreen, BYTE *rgbtBlue)
     *rgbtBlue = averaged_rounded;
 }
 
+// Convert image to grayscale
 void grayscale(int height, int width, RGBTRIPLE image[height][width])
 {
     // printf("Height: %i - width: %i\n", height, width);
@@ -44,24 +60,14 @@ void PixelReflected(int row, int column, int width, RGBTRIPLE image[][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
-    // FILE *af = fopen("after.txt", "w");
-    // FILE *bf = fopen("before.txt", "w");
     for (int row=0; row < height; row++)
     {
-        // printf("At row: %i ...\n", row);
         for (int column=0; column < width/2; column++)
         {
-            // printf("BEFORE SWITCH INSIDE REFLECT ...\n");
-            // printf("Temp Red: %d...\n", image[row][column].rgbtRed);
-            // printf("Temp Green: %d...\n", image[row][column].rgbtGreen);
-            // printf("Temp Blue: %d...\n\n", image[row][column].rgbtBlue);
-            // fprintf(bf, "(%i)=%i-%i-%i,",column, image[row][column].rgbtRed, image[row][column].rgbtGreen, image[row][column].rgbtBlue);
             PixelReflected(row, column, width, image);
-            // fprintf(af, "(%i)=%i-%i-%i,",width-column-1, image[row][column].rgbtRed, image[row][column].rgbtGreen, image[row][column].rgbtBlue);
         }
     }
-    // fclose(af);
-    // fclose(bf);
+
     return;
 }
 
@@ -120,8 +126,83 @@ void blur(int height, int width, RGBTRIPLE image[height][width])
     return;
 }
 
+RGBTRIPLE DetectEdges(int row, int column, int height, int width, RGBTRIPLE image[][width], RGBTRIPLE temp[][width])
+{
+    int unsigned bottom = (row + 1) >= height ? height-1 : (row + 1);
+    int unsigned top = (row - 1) < 0 ? 0 : (row - 1);
+
+    int unsigned right = (column + 1) >= width ? width-1 : (column + 1);
+    int unsigned left = (column - 1) < 0 ? 0: (column - 1);
+
+    float Gx_sum_red = 0;
+    float Gx_sum_green = 0;
+    float Gx_sum_blue = 0;
+
+    float Gy_sum_red = 0;
+    float Gy_sum_green = 0;
+    float Gy_sum_blue = 0;
+
+    float combined_red, combined_green, combined_blue;
+
+    for (int i=0;  i <= bottom - top; i++)
+    {
+        for (int j = 0; j <= right - left; j++)
+        {
+            if (top + i != row || left + j != column)
+            {
+                Gx_sum_red += image[top+i][left+j].rgbtRed * Gx[i][j];
+                Gx_sum_green += image[top+i][left+j].rgbtGreen * Gx[i][j];
+                Gx_sum_blue += image[top+i][left+j].rgbtBlue * Gx[i][j];
+                
+                Gy_sum_red += image[top+i][left+j].rgbtRed * Gy[i][j];
+                Gy_sum_green += image[top+i][left+j].rgbtGreen * Gy[i][j];
+                Gy_sum_blue += image[top+i][left+j].rgbtBlue * Gy[i][j];
+            }
+        }
+    }
+
+    combined_red = (int)round(sqrt(pow(Gx_sum_red, 2) + pow(Gy_sum_red, 2)));
+    combined_green = (int)round(sqrt(pow(Gx_sum_green, 2) + pow(Gy_sum_green, 2)));
+    combined_blue = (int)round(sqrt(pow(Gx_sum_blue, 2) + pow(Gy_sum_blue, 2)));
+
+    if (combined_red > 255)
+    {
+        combined_red = 255;
+    }
+    if (combined_green > 255)
+    {
+        combined_green = 255;
+    }
+    if (combined_blue > 255)
+    {
+        combined_blue = 255;
+    }
+    
+    temp[row][column].rgbtRed = combined_red;
+    temp[row][column].rgbtGreen = combined_green;
+    temp[row][column].rgbtBlue = combined_blue;
+
+    return temp[row][column];
+}
+
 // Detect edges
 void edges(int height, int width, RGBTRIPLE image[height][width])
 {
+    RGBTRIPLE(*temp)[width] = calloc(height, width * sizeof(RGBTRIPLE));
+    for (int row=0; row < height; row++)
+    {
+        for (int column=0; column < width; column++)
+        {
+            temp[row][column] = DetectEdges(row, column, height, width, image, temp);
+        }
+    }
+    
+    for (int i=0; i < height; i++)
+    {
+        for (int j=0; j < width; j++)
+        {
+            image[i][j] = temp[i][j];
+        }
+    }
     return;
 }
